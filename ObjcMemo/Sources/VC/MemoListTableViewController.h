36f5003bcae631f6fd41b3fdd00bbd31657cafcf
김대희 <0824dh@naver.com>
2021-12-18 14:30:19 +0900

    Add :: MemoListTableViewController File

---
+++ ObjcMemo/Sources/VC/MemoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MemoListTableViewController.h
+//  ObjcMemo
+//
+//  Created by 김대희 on 2021/12/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MemoListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
