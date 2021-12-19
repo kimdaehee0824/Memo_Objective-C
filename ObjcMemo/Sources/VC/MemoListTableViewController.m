a0e139ed98d65998416654ffb3d17866069d0ac3
김대희 <0824dh@naver.com>
2021-12-19 21:19:43 +0900

    Add :: tableView Data List 구현

--- ObjcMemo/Sources/VC/MemoListTableViewController.m
+++ ObjcMemo/Sources/VC/MemoListTableViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "MemoListTableViewController.h"
+#import "Memo.h"
 
 @interface MemoListTableViewController ()
 
@@ -15,35 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return [[Memo dummyMemoList] count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    Memo* target = [[Memo dummyMemoList]objectAtIndex:indexPath.row];
+    cell.textLabel.text = target.content;
+    cell.detailTextLabel.text = target.insertDate.description;
+    
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
