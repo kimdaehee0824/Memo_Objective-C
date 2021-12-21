3dfd7d0250f34c0e0d616b15958fdc4803faebf7
김대희 <0824dh@naver.com>
2021-12-21 14:49:21 +0900

    Add :: data 전달

--- ObjcMemo/Sources/VC/DetailViewController.m
+++ ObjcMemo/Sources/VC/DetailViewController.m
@@ -10,13 +10,19 @@
 @interface DetailViewController ()
 
 <UITableViewDataSource>
+@property (strong, nonatomic) NSDateFormatter* formatter;
 
 @end
+
 
 @implementation DetailViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.formatter = [[NSDateFormatter alloc] init];
+    self.formatter.dateStyle = NSDateFormatterLongStyle;
+    self.formatter.timeStyle = NSDateFormatterMediumStyle;
+    self.formatter.locale = [NSLocale localeWithLocaleIdentifier:@"Ko_kr"];
     // Do any additional setup after loading the view.
 }
 
@@ -29,10 +35,12 @@
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     if (indexPath.row == 0) {
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"memoCell" forIndexPath:indexPath];
+        cell.textLabel.text = self.memo.content;
         return cell;
     }
     else {
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
+        cell.textLabel.text = [self.formatter stringFromDate:self.memo.insertDate];
         return cell;
     }
 }
