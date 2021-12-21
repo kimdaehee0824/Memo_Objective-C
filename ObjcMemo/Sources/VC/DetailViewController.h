63faa03423a56a8fad50738b3ab4dc18fa7092a4
김대희 <0824dh@naver.com>
2021-12-21 13:55:52 +0900

    Add DetailViewController & StoryBoard

---
+++ ObjcMemo/Sources/VC/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ObjcMemo
+//
+//  Created by 김대희 on 2021/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjcMemo/Sources/VC/DetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.m
+//  ObjcMemo
+//
+//  Created by 김대희 on 2021/12/21.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
